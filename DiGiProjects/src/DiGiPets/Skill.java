036e4ec93a1fd808267244e8431f58ce5721359d
WeLikeAich <jaichler13@apu.edu>
2014-12-15 20:50:21 -0800

    Changed Skill class to enum Added test skills

--- DiGiProjects/src/DiGiPets/Skill.java
+++ DiGiProjects/src/DiGiPets/Skill.java
@@ -1,6 +1,32 @@
 package DiGiPets;
 
-public class Skill {
-//Testing this shit.
-	//tihs siht gnitest
+public enum Skill 
+{
+	//Test Skills
+	Wave("Waves to screen", "Social"),
+	Bow("Bows to screen", "Social");
+	
+	//Skill information variables
+	private final static String desc;
+	private final static String type;
+	
+	//Skill enum constructor
+	Skill(String Description, String skillType)
+	{
+		desc = Description;
+		type = skillType;
+	}
+	
+	/*
+	 * Skill information Accessors
+	 */
+	public String getDesc()
+	{
+		return desc;
+	}
+	
+	public String getType()
+	{
+		return skillType;
+	}
 }
